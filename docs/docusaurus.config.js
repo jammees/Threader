bc85e5f1bb571d12b133165d36f37e9aa9319583
James <86011536+jammees@users.noreply.github.com>
2023-11-26 00:35:58 +0100

    ?????

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Threader',
   trailingSlash: false,
 
   // GitHub pages deployment config.
