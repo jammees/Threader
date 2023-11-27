1fe5ad4e69ea231032736a82e60e7fb5418e5862
James <86011536+jammees@users.noreply.github.com>
2023-11-27 17:44:57 +0100

    Remove Threader

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/SVG Threader logo short light.svg',
 
   // Set the production url of your site here
-  url: 'https://jammees.github.io/Threader/',
+  url: 'https://jammees.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Threader',
