8383f37a9ecf874b09b071c4893abe2183ecfb39
James <86011536+jammees@users.noreply.github.com>
2023-11-27 17:43:35 +0100

    Fix url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/SVG Threader logo short light.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jammees.github.io/Threader/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Threader',
